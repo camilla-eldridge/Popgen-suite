116282659e03d360a3f33eba2eb0b2c1a4af67ec
Camilla Eldridge <k1116370@kingston.ac.uk>
2022-04-14 22:55:02 +0100

    updated Relative_geno_pheno_plots.R

--- Relative_frequency_plots/Relative_geno_pheno_plots.R
+++ Relative_frequency_plots/Relative_geno_pheno_plots.R
@@ -1,14 +1,14 @@
 #!/usr/bin/env Rscript
 
 #author: camilla eldridge
-
-
-
 args = commandArgs(trailingOnly=TRUE)
 
 library(ggplot2)
 library(gridExtra)
 library(ggpubr)
+library(pegas)
+require(ape)
+
 
 # Input as csv output from cluster_finder.py #
 cluster_members_nucl<-args[1]
@@ -17,24 +17,28 @@ cluster_info_nucl<-args[3]
 cluster_info_prot<-args[4]
 groups<-args[5]
 group_name<-args[6]
-# 
+cds_seq<-args[7]
+
 # # Allele ID for each individual in population.
-# cluster_members_prot<-"test_nucl_cluster_members.csv"
+#cluster_members_prot<-"test_nucl_cluster_members.csv"
 # 
 # # Protein ID for each individual in population.
-# cluster_members_nucl<-"test_prot_cluster_members.csv"
+#cluster_members_nucl<-"test_prot_cluster_members.csv"
 # 
 # # Whole pop allele counts.
-# cluster_info_nucl<-"test_nucl_cluster_info.txt" 
+#cluster_info_nucl<-"test_nucl_cluster_info.txt" 
 # 
 # # Whole pop protein counts. 
-# cluster_info_prot<-"test_prot_cluster_info.txt"
+#cluster_info_prot<-"test_prot_cluster_info.txt"
 # 
 # # Info on group assignment for each individual 
-# groups<-"group_info.csv"
+#groups<-"group_info.csv"
 # 
 # # name of group for plotting and table generation
-# group_name="Host"
+#group_name="Host"
+
+# fasta sequences  
+#cds_seq<-"test.fasta"
 
 
 ####################        Part I          #################################
@@ -177,15 +181,14 @@ result3 <- write.csv(x=Protein_group_percs, file="group_protein_counts.csv", row
 
 
 # ####################        Part III          #################################
-
 # colour blind palette for gglot2
 cbPalette <- c("#0072B2", "#D55E00", "#CC79A7","#56B4E9", "#009E73", "#F0E442")
  
 # reorder by perc for plotting
 reord_A<-Allele_group_percs[order(Allele_group_percs$Perc_alleles),]
 
-attach(reord_A
-       )
+attach(reord_A)
+
 # string for x axis variable 
 x_axis<- paste(group_name)
 
@@ -199,9 +202,7 @@ Relative_allele_plot<-ggplot() +
 detach(reord_A)
 
 
-
 # make plot for proteins
-
 # reorder by perc for plotting
 reord_P<-Protein_group_percs[order(Protein_group_percs$Perc_proteins),]
 
@@ -214,11 +215,121 @@ Relative_prot_plot<-ggplot() +
   scale_fill_manual("Protein", values = cbPalette)
 
 
+
+
+########### AMOVA, TAJD ############
+library(pegas)
+
+# All individuals sequenced #
+indiv_seqs<-read.dna(cds_seq, format = "fasta", skip = 0, as.character = FALSE, as.matrix = TRUE)
+class(indiv_seqs)
+
+#Tajima with p value (pegas) 
+t<-tajima.test(indiv_seqs)
+
+# gc content
+gc<-GC.content(indiv_seqs)
+
+#nucleotide diversity
+d<-nuc.div(indiv_seqs, variance = FALSE, pairwise.deletion = FALSE)
+
+#haplotype diversity
+h<-hap.div(indiv_seqs)
+
+#haplotype number
+hn<-haplotype(indiv_seqs)
+
+# ape to find base frequencies #
+bf<-base.freq(indiv_seqs)
+
+# ramos onsins rozas test
+Ra<-R2.test(indiv_seqs)
+
+
+
+# plot arrangement
+jpeg("summary.jpg")
+
+par(mfrow = c(2,2))
+
+# call r2 plot 
+#Ra
+R2.test(indiv_seqs)
+
+# plot of haplotype freqs
+plot(haplotype(indiv_seqs), main = "Haplotype distribution")
+
+# Site frequency spectrum
+sp<-site.spectrum(indiv_seqs)
+plot(sp, col = "lightgreen", main = "SFS")
+#Wakeley, J. (2009) Coalescent Theory: An Introduction. Greenwood Village, CO: Roberts and Company Publishers.
+
+# R2 test -  Ramos-Onsins-Rozas Test of Neutrality
+#R2.test(indiv_seqs)
+#Ramos-Onsins, R. and Rozas, R. (2002) Statistical properties of new neutrality tests against population growth. Molecular Biology and Evolution, 19, 2092–2100.
+#Sano, J. and Tachida, G. (2005) Gene genealogy and properties of test statistics of neutrality under population growth. Genetics, 169, 1687–1697.
+
+# pairwise mismatch 
+MMD(indiv_seqs, col = "grey", breaks = 20, legend = FALSE, lty = 1:2, main = "Pairwise mismatch")
+# blue = empirical
+# red = stable expectation
+#Rogers, A. R. and Harpending, H. (1992) Population growth makes waves in the distribution of pairwise genetic-differences. Molecular Biology and Evolution, 9, 552–569.
+
+dev.off()
+
+######### summary tables ######
+
+df.summary<-cbind(t[1], t[2],d,h,Ra[1], Ra[2],bf[1],bf[2], bf[3], bf[4])
+colnames(df.summary)<-c("D", "D.pval", "nuc.div", "hap.div", "r2", "r2.pval", "a", "c", "g", "t")
+row.names(df.summary)<-NULL
+
+############  AMOVA #############################
+## genetic distance matrix for all individuals ##
+indiv_seqs.dist <- dist(x = indiv_seqs, method = "euclidean", diag=TRUE, upper=TRUE)
+
+# order of input sequences stays the same so we can use Host variable for AMOVA.
+Host<-Merged_ordered$Host  # from groups<-"group_info.csv"
+
+AMOVA<-amova(indiv_seqs.dist ~ Host, nperm = 7000)
+
+#sum of squares deviation, mean square deviation 
+AMOVA$tab
+
+# variancecoeff (a)
+AMOVA$varcoef
+
+# variance componants (sigma2, pvalue)
+AMOVA$varcomp
+
+#phiST
+PhiST <- 1 - AMOVA$varcoef/(AMOVA$varcoef+AMOVA$varcomp[2,1])
+
+
+# AMOVA tables for ggplot #
+df.amova<-data.frame(AMOVA$varcomp)
+ss <- tableGrob(df.amova)
+
+f<-tableGrob(AMOVA$tab)
+
+phi.varco<-rbind(as.numeric(PhiST), as.numeric(AMOVA$varcoef))
+rownames(phi.varco)<-c("PhiST", "var.coef")
+colnames(phi.varco)<-NULL
+
+k <- tableGrob(phi.varco)
+
+sums<-tableGrob(df.summary)
+
+
+
 # arrange in grid 
-ggarrange(Relative_allele_plot,Relative_prot_plot, labels = c('I','II'), hjust = 0.1, vjust =1.2, ncol = 2, nrow = 1)
+p1<-ggarrange(Relative_allele_plot,Relative_prot_plot, labels = c('I','II'), hjust = 0.1, vjust =1.2, ncol = 2, nrow = 1)
+p2<-ggarrange(ss,f,k, ncol = 1, nrow = 3, labels = "III")
+p3<-
+
+ggarrange(p1, p2, ncol = 1, nrow = 2)
 
 
 # save plots
-ggsave("Relative_phen_geno_plot.png", device = "png", width = 25, height = 10, units = "cm")
+ggsave("Relative_phen_geno_plot_amova.png", device = "png", width = 25, height = 25, units = "cm")
 
 
